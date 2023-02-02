556f5fe8e49516f8e7c2db6634b4bd29e28d73e5
Miguel Pascual Tenza <98650471+miguepascualtenza@users.noreply.github.com>
2023-02-02 20:22:39 +0100

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,16 +1,33 @@
+ /* Inicio Miguel Pascual */
+ /* Carousel */
+  const imagenes = document.querySelectorAll('.imagen'); //array con todas las imagenes
+  let imagenActual = 0; //sigue el array de imagenes
+
+  function mostrarImagen() {
+    imagenes.forEach((imagen) => {
+     imagen.style.display = 'none'; //oculta todas las imagenes
+    });
+    imagenes[imagenActual].style.display = 'block'; //muestra solo la que la imagenActual dice
+  }
+
+  mostrarImagen();
+
+  setInterval(() => {
+    imagenActual = (imagenActual + 1) % imagenes.length; //pasa a la siguente imagen cada 2seg
+    mostrarImagen();
+  }, 2000);
 
 
-
-/* Carousel */
-const imagenes = document.querySelectorAll('.imagen'); //array con todas las imagenes
-let imagenActual = 0; //sigue el array de imagenes
-
-function mostrarImagen() {
-  imagenes.forEach((imagen) => {
-   imagen.style.display = 'none'; //oculta todas las imagenes
+  /* menu responsibe */
+  document.querySelector(".menu-btn").addEventListener("click", () => {
+  document.querySelector(".nav-menu").classList.toggle("show");
   });
-  imagenes[imagenActual].style.display = 'block'; //muestra solo la que la imagenActual dice
-}
+
+  document.querySelector(".menu-btn").addEventListener("click", () => {
+  document.querySelector(".iconos").classList.toggle("show");
+  });
+   /* Final Miguel Pascual */
+
 
 mostrarImagen();
 
