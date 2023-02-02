fad2728a09e08b323d7dd2d3854b2ca8f8705f92
Miguel Pascual Tenza <98650471+miguepascualtenza@users.noreply.github.com>
2023-02-02 19:42:42 +0100

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,48 +1,78 @@
-var slideIndex = 1;
-showSlides(slideIndex);
 
-function prevSlide() {
-  showSlides(slideIndex -= 1);
+
+
+/* Carousel */
+const imagenes = document.querySelectorAll('.imagen'); //array con todas las imagenes
+let imagenActual = 0; //sigue el array de imagenes
+
+function mostrarImagen() {
+  imagenes.forEach((imagen) => {
+   imagen.style.display = 'none'; //oculta todas las imagenes
+  });
+  imagenes[imagenActual].style.display = 'block'; //muestra solo la que la imagenActual dice
 }
 
-function nextSlide() {
-  showSlides(slideIndex += 1);
+mostrarImagen();
+
+setInterval(() => {
+  imagenActual = (imagenActual + 1) % imagenes.length; //pasa a la siguente imagen cada 2seg
+  mostrarImagen();
+}, 2000);
+
+// Obtenir l'idioma de la URL si existeix i executar la funció "changeLanguage"
+const querystring = window.location.search; // Si la url és: https://miweb.es?lang=es , retorna ?lang=es
+const urlParams = new URLSearchParams(querystring);
+if (urlParams.get('lang')){
+  const lang = urlParams.get('lang');
+  changeLanguage(lang);
 }
 
-function currentSlide(n) {
-  showSlides(slideIndex = n);
+async function changeLanguage(lang) {
+  // Guardar l'idioma a la URL
+  const newURL = window.location.protocol + '//' + window.location.host + window.location.pathname + '?lang=' + lang;
+  window.history.replaceState({}, '', newURL);
+
+  // Obtenir el fitxer json corresponent
+  const response = await fetch(`../I18N/${lang}.json`);
+  const data = await response.json();
+
+
+
+  // Canviar els texts (Aquesta és la part que heu de modificar)
+
+  console.log(document);
+  console.log(document.getElementById('first_name'));
+  document.getElementById('first_name').placeholder = data.first_name;
+  document.getElementById('warning_first_name').innerHTML = data.warning_first_name;
+
+  
+
+  console.log(document);
+  console.log(document.getElementById('last_name'));
+  document.getElementById('last_name').placeholder = data.last_name;
+
+  console.log(document);
+  console.log(document.getElementById('email'));
+  document.getElementById('email').placeholder = data.email;
+
+  console.log(document);
+  console.log(document.getElementById('Password'));
+  document.getElementById('Password').placeholder = data.Password;
+
+  console.log(document);
+  console.log(document.getElementById('inicio_sesion'));
+  document.getElementById('inicio_sesion').value = data.inicio_sesion;
+
+  console.log(document);
+  console.log(document.getElementById('novedades'));
+  document.getElementById('novedades').innerHTML = data.novedades;
+
+  console.log(document);
+  console.log(document.getElementById('mujer'));
+  document.getElementById('mujer').innerHTML = data.mujer;
+
+
+
+
 }
 
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("carousel-slide");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-  }
-  slides[slideIndex-1].style.display = "block";
-}
-
-// Selecciona la imagen
-const miImagen = document.querySelector('.mi-imagen');
-const imagenes = document.querySelectorAll('.imagenes-ocultas img');
-let contador = 0;
-
-// Establece el intervalo de tiempo para el cambio de imagen
-const interval = setInterval(() => {
-    miImagen.src = imagenes[contador].src;
-    contador++;
-    if (contador === 3){
-        clearInterval(interval);
-    }
-    // Si se ha llegado al final del array, vuelve a la primera imagen
-    if (contador === imagenes.length) {
-        contador = 0;
-    }
-}, 1000);
-
-// Detiene el intervalo al salir el cursor
-miImagen.addEventListener('mouseout', () => {
-    clearInterval(interval);
-});
